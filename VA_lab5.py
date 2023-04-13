27b219eef73b0468673989cee2c1f4c1733607f0
leriashes <valeriya-shevchenko-2002@mail.ru>
2023-04-13 23:18:21 +0700

    Метод правых прямоугольников

--- VA_lab5.py
+++ VA_lab5.py
@@ -3,6 +3,33 @@ import matplotlib.pyplot as plt
 import math
 import getch
 
+#вычисление значений функции
+def count_function(xs, func):
+    ys = []
+
+    for i in range(len(xs)):
+        x = xs[i]
+        ys.append(eval(func))
+    return ys
+
+def rectangleRight(ys, h):
+
+    result = 0
+
+    for i in range (len(ys) - 1):
+        result += ys[i]
+
+    result *= h
+
+    return result
+
+def rectangleMiddle(func, a, b, h):
+    return
+
+def rectangleLeft(func, a, b, h):
+    return
+
+
 while True:
     print('Введите функцию: y = ', end='')
     func = input()
@@ -20,6 +47,14 @@ while True:
         if b > a:
             break
 
+    print('Введите шаг: h = ', end='')
+    h = float(input())
+    print('\n')
+
+    xs = np.arange(a, b + h, h)
+    ys = count_function(xs, func)
+
+    print('Метод правых прямоугольников: ', rectangleRight(ys, h))
 
     print('\n\nЧтобы продолжить нажмите Enter. Для выхода из программы нажмите любую другую клавишу. ', end='')
     cont = getch.getch()
